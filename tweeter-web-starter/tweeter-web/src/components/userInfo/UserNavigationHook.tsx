370fcf2204eb55c5c3375205bccf7307487705f4
Lily Hill <lillianmaihill@gmail.com>
2025-02-14 04:27:09 -0700

    UserNavigation Hook mvp

--- tweeter-web-starter/tweeter-web/src/components/userInfo/UserNavigationHook.tsx
+++ tweeter-web-starter/tweeter-web/src/components/userInfo/UserNavigationHook.tsx
@@ -1,42 +1,19 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
 import userInfoHook from "./UserInfoHook";
 import useToastListener from "../toaster/ToastListenerHook";
+import { UserNavigationPresenter, UserNavigationView } from "../../presenters/UserNavigationPresenter";
 
 const useUserNavigationHook = () => {
     const { setDisplayedUser, authToken, currentUser } = userInfoHook();
     const { displayErrorMessage } = useToastListener();
 
+    const listener: UserNavigationView = {
+        displayErrorMessage: displayErrorMessage,
+        setDisplayedUser: setDisplayedUser
+    }
+    const presenter = new UserNavigationPresenter(listener);
+
     const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-        event.preventDefault();
-
-        try {
-            const alias = extractAlias(event.target.toString());
-
-            const user = await getUser(authToken!, alias);
-
-            if (!!user) {
-                if (currentUser!.equals(user)) {
-                    setDisplayedUser(currentUser!);
-                } else {
-                    setDisplayedUser(user);
-                }
-            }
-        } catch (error) {
-            displayErrorMessage(`Failed to get user because of exception: ${error}`);
-        }
-    };
-    
-    const extractAlias = (value: string): string => {
-        const index = value.indexOf("@");
-        return value.substring(index);
-    };
-
-    const getUser = async (
-        authToken: AuthToken,
-        alias: string
-    ): Promise<User | null> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.findUserByAlias(alias);
+        presenter.navigateToUser(event, authToken, currentUser);
     };
       
     return (
