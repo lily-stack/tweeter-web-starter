13e79339af4721dd3816498d274cf6e7a6798382
Lily Hill <lillianmaihill@gmail.com>
2025-02-14 05:01:21 -0700

    logout mvp

---
+++ tweeter-web-starter/tweeter-web/src/presenters/LogoutPresenter.ts
@@ -0,0 +1,34 @@
+import { AuthToken } from "tweeter-shared";
+import { UserService } from "../model/service/UserService";
+
+export interface LogoutView {
+    displayInfoMessage: (message: string, duration: number, bootstrapClasses?: string) => void;
+    clearLastInfoMessage: () => void;
+    displayErrorMessage: (message: string, bootstrapClasses?: string) => void;
+    clearUserInfo: () => void;
+
+}
+export class LogoutPresenter {
+    private view: LogoutView;
+    private userService: UserService;
+
+    public constructor(view: LogoutView) {
+        this.view = view;
+        this.userService = new UserService();
+    }
+
+    public async logOut (authToken: AuthToken | null) {
+        this.view.displayInfoMessage("Logging Out...", 0);
+    
+        try {
+          await this.userService.logout(authToken!);
+    
+          this.view.clearLastInfoMessage();
+          this.view.clearUserInfo();
+        } catch (error) {
+          this.view.displayErrorMessage(
+            `Failed to log user out because of exception: ${error}`
+          );
+        }
+    };
+}
